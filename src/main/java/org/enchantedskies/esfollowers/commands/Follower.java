19b05d7b134163962dc29f0b08723b57ed26e647
BuildTools <unconfigured@null.spigotmc.org>
2020-12-25 01:40:22 +0000

    Added more functionality to code and made movement smoother

--- src/main/java/org/enchantedskies/esfollowers/commands/Follower.java
+++ src/main/java/org/enchantedskies/esfollowers/commands/Follower.java
@@ -1,26 +1,21 @@
 package org.enchantedskies.esfollowers.commands;
 
-import com.destroystokyo.paper.entity.Pathfinder;
 import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Bee;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.EulerAngle;
 import org.bukkit.util.Vector;
-import org.bukkit.util.noise.SimplexOctaveGenerator;
 import org.enchantedskies.esfollowers.CharacterArmorStand;
 import org.enchantedskies.esfollowers.ESFollowers;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.UUID;
-import java.util.concurrent.Executors;
 
 public class Follower implements CommandExecutor {
     ESFollowers plugin = ESFollowers.getPlugin(ESFollowers.class);
@@ -32,44 +27,53 @@ public class Follower implements CommandExecutor {
             return true;
         }
         Player player = (Player) sender;
-        World world = player.getWorld();
-        petMovement(player, 0.1);
-//        Bee bee = world.spawn(player.getLocation(), Bee.class);
-//        bee.setBaby();
-//        bee.setSilent(true);
-//        bee.setInvulnerable(true);
-//        bee.setInvisible(true);
-//        armorStandConnector(player, bee);
+        petMovement(player, 0.4);
         player.sendMessage(ChatColor.GREEN + "Pet Spawned.");
-//        movementRunnable(player, bee, false);
         return true;
     }
 
     public void petMovement(Player player, double speed) {
-        CharacterArmorStand characterArmorStand = new CharacterArmorStand(player.getLocation(), getPlayerSkull(player), getItemStack(Material.LEATHER_CHESTPLATE), getItemStack(Material.LEATHER_LEGGINGS), getItemStack(Material.LEATHER_BOOTS));
+        CharacterArmorStand characterArmorStand = new CharacterArmorStand(player.getLocation(), getPlayerSkull(UUID.fromString("04557d52-6c3f-4053-b86c-5067d2720931")), getColouredArmour(Material.LEATHER_CHESTPLATE, "#9D9D97"), getColouredArmour(Material.LEATHER_LEGGINGS, "#897362"), getColouredArmour(Material.LEATHER_BOOTS, "#B02E26"));
         ArmorStand armorStand = characterArmorStand.getArmorStand();
         new BukkitRunnable() {
             public void run() {
                 Location petLoc = armorStand.getLocation();
                 Vector difference = getDifference(player, armorStand);
-                if (difference.lengthSquared() >= 2.25) {
-                    Vector normalizedDifference = difference.normalize();
+                if (difference.clone().setY(0).lengthSquared() < 6.25) {
+                    Vector differenceY = difference.clone().setX(0).setZ(0);
+                    petLoc.add(differenceY.multiply(speed));
+                } else {
+                    Vector normalizedDifference = difference.clone().normalize();
                     petLoc.add(normalizedDifference.multiply(speed));
                 }
+
+                if (difference.lengthSquared() > 1024) {
+                    armorStand.teleport(player);
+                    return;
+                }
+
                 petLoc.setDirection(difference);
                 armorStand.teleport(petLoc.add(0, getArmorStandYOffset(armorStand), 0));
-                armorStand.setHeadPose(new EulerAngle(getPitch(player, armorStand), 0, 0));
+                double headPoseX = eulerToDegree(armorStand.getHeadPose().getX());
+                EulerAngle newHeadPoseX = new EulerAngle(getPitch(player, armorStand), 0, 0);
+                if (headPoseX > 60 && headPoseX < 290) {
+                    if (headPoseX <= 175) {
+                        newHeadPoseX.setX(60D);
+                    } else {
+                        newHeadPoseX.setX(290D);
+                    }
+                }
+                armorStand.setHeadPose(newHeadPoseX);
             }
         }.runTaskTimer(plugin, 0L, 1L);
     }
 
     public double getArmorStandYOffset(ArmorStand armorStand) {
-        double y = (Math.PI / 240) * Math.sin(((double) 1/60) * Math.PI * (Bukkit.getCurrentTick() + armorStand.getEntityId()));
-        return y;
+        return (Math.PI / 60) * Math.sin(((double) 1/30) * Math.PI * (Bukkit.getCurrentTick() + armorStand.getEntityId()));
     }
 
     public Vector getDifference(Player player, ArmorStand armorStand) {
-        return player.getLocation().subtract(armorStand.getLocation()).toVector();
+        return player.getEyeLocation().subtract(armorStand.getEyeLocation()).toVector();
     }
 
     public double getPitch(Player player, ArmorStand armorStand) {
@@ -90,7 +94,33 @@ public class Follower implements CommandExecutor {
         return skullItem;
     }
 
+    public ItemStack getPlayerSkull(UUID uuid) {
+        ItemStack skullItem = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta skullMeta = (SkullMeta) skullItem.getItemMeta();
+        if (skullMeta == null) return skullItem;
+        skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(uuid));
+        skullItem.setItemMeta(skullMeta);
+        return skullItem;
+    }
+
+    public ItemStack getColouredArmour(Material material, String hexColour) {
+        ItemStack item = new ItemStack(material);
+        ItemMeta itemMeta = item.getItemMeta();
+        if (!(itemMeta instanceof LeatherArmorMeta)) return item;
+        LeatherArmorMeta armorMeta = (LeatherArmorMeta) item.getItemMeta();
+        int red = Integer.valueOf(hexColour.substring(1, 3), 16 );
+        int green = Integer.valueOf(hexColour.substring(3, 5), 16 );
+        int blue = Integer.valueOf(hexColour.substring(5, 7), 16 );
+        armorMeta.setColor(Color.fromRGB(red, green, blue));
+        item.setItemMeta(armorMeta);
+        return item;
+    }
+
     public ItemStack getItemStack(Material material) {
         return new ItemStack(material);
+    }
+
+    public double eulerToDegree(double euler) {
+        return (euler / (2 * Math.PI)) * 360;
     }
 }
