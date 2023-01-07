7386277bd32854d04ced2fc0725e221179af9310
BuildTools <cooldaveb@live.co.uk>
2023-01-07 16:42:51 +0000

    Updated GSit API

--- src/main/java/me/dave/followers/apis/GSitEvents.java
+++ src/main/java/me/dave/followers/apis/GSitEvents.java
@@ -1,9 +1,6 @@
 package me.dave.followers.apis;
 
-import dev.geco.gsit.api.event.PlayerGetUpPoseEvent;
-import dev.geco.gsit.api.event.PlayerGetUpSitEvent;
-import dev.geco.gsit.api.event.PlayerPoseEvent;
-import dev.geco.gsit.api.event.PlayerSitEvent;
+import dev.geco.gsit.api.event.*;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Pose;
 import org.bukkit.event.EventHandler;
@@ -14,19 +11,21 @@ import me.dave.followers.FollowerEntity;
 public class GSitEvents implements Listener {
 
     @EventHandler
-    public void onPlayerSit(PlayerSitEvent event) {
-        Player player = event.getPlayer();
-        FollowerEntity follower = Followers.dataManager.getPlayerFollowerMap().get(player.getUniqueId());
-        if (follower == null) return;
-        follower.setPose("sitting");
+    public void onPlayerSit(EntitySitEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            FollowerEntity follower = Followers.dataManager.getPlayerFollowerMap().get(player.getUniqueId());
+            if (follower == null) return;
+            follower.setPose("sitting");
+        }
     }
 
     @EventHandler
-    public void onPlayerGetUpFromSeat(PlayerGetUpSitEvent event) {
-        Player player = event.getPlayer();
-        FollowerEntity follower = Followers.dataManager.getPlayerFollowerMap().get(player.getUniqueId());
-        if (follower == null) return;
-        follower.setPose("default");
+    public void onPlayerGetUpFromSeat(EntityGetUpSitEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            FollowerEntity follower = Followers.dataManager.getPlayerFollowerMap().get(player.getUniqueId());
+            if (follower == null) return;
+            follower.setPose("default");
+        }
     }
 
     @EventHandler
