a5f28a7f593d3036d38ce40365d92663ca270a44
Oak <cooldaveb@live.co.uk>
2024-05-27 22:23:42 +0100

    Updated comments and added TODO

--- src/main/java/org/lushplugins/followers/data/FollowerHandler.java
+++ src/main/java/org/lushplugins/followers/data/FollowerHandler.java
@@ -120,7 +120,8 @@ public class FollowerHandler {
     public void applyAttributes(WrapperLivingEntity entity) {
         LivingEntityMeta entityMeta = (LivingEntityMeta) entity.getEntityMeta();
         entityMeta.setInvisible(!isVisible);
-        entityMeta.setSilent(true);
+        // TODO: Uncomment when implemented in EntityLib
+//        entityMeta.setSilent(true);
         entity.getAttributes().setAttribute(Attributes.GENERIC_SCALE, scale);
 
         if (entityMeta instanceof ArmorStandMeta armorStandMeta) {
