417269469871aa4154176d7ecda8d50ec26c2d2a
Oak <cooldaveb@live.co.uk>
2024-05-28 18:18:47 +0100

    Improved movement and rotation calculations

--- src/main/java/org/lushplugins/followers/entity/tasks/MovementTask.java
+++ src/main/java/org/lushplugins/followers/entity/tasks/MovementTask.java
@@ -8,17 +8,11 @@ import me.tofaa.entitylib.meta.other.ArmorStandMeta;
 import me.tofaa.entitylib.wrapper.WrapperLivingEntity;
 import org.lushplugins.followers.Followers;
 import org.lushplugins.followers.entity.FollowerEntity;
-import org.bukkit.entity.Player;
-import org.lushplugins.lushlib.utils.Pair;
 
 public class MovementTask extends FollowerTask {
-    private final Player player;
-    private final double speed;
 
     public MovementTask(FollowerEntity followerEntity) {
         super(followerEntity);
-        this.player = followerEntity.getPlayer();
-        this.speed = Followers.getInstance().getConfigManager().getSpeed();
     }
 
     @Override
@@ -29,47 +23,37 @@ public class MovementTask extends FollowerTask {
             return;
         }
 
+        // TODO: Replace player with target entity (Potentially implement as separate task)
+        followerEntity.setTarget(SpigotConversionUtil.fromBukkitLocation(followerEntity.getPlayer().getLocation()));
+
         WrapperLivingEntity entity = followerEntity.getEntity();
         Location currentLocation = entity.getLocation();
         Vector3d newPosition = calculatePosition();
 
-        float yaw;
-        float pitch;
-        // Limits following code to run once every 2 ticks
+        // Calculations limited to running once every 2 ticks
+        Vector3f rotation;
         if (Followers.getInstance().getCurrentTick() % 2 == 0) {
-            Pair<Float, Float> rotation = calculateRotation();
-            yaw = rotation.first();
-            pitch = rotation.second();
+            rotation = calculateRotation();
         } else {
-            yaw = currentLocation.getYaw();
-            pitch = currentLocation.getPitch();
+            rotation = new Vector3f(currentLocation.getPitch(), currentLocation.getYaw(), 0);
         }
 
-        followerEntity.teleport(new Location(newPosition, yaw, pitch));
+        followerEntity.teleport(new Location(newPosition, rotation.getY(), rotation.getX()));
+        entity.rotateHead(rotation.getY(), rotation.getX());
 
-        // Limits following code to run once every 2 ticks
-        if (Followers.getInstance().getCurrentTick() % 2 == 0) {
-            if (entity.getEntityMeta() instanceof ArmorStandMeta armorStandMeta) {
-                // Sets follower head to be looking at the player
-                double headPoseX = eulerToDegree(armorStandMeta.getHeadRotation().getX());
-                Vector3f newHeadPoseX;
-                if (headPoseX > 60 && headPoseX < 290) {
-                    newHeadPoseX = new Vector3f(headPoseX <= 175 ? 60 : 290, 0, 0);
-                } else {
-                    newHeadPoseX = new Vector3f(getPitch(player, entity), 0, 0);
-                }
-
-                armorStandMeta.setHeadRotation(newHeadPoseX);
-            }
+        if (entity.getEntityMeta() instanceof ArmorStandMeta armorStandMeta) {
+            Vector3f rotationRadians = new Vector3f((float) Math.toRadians(rotation.getX()), (float) Math.toRadians(rotation.getY()), (float) Math.toRadians(rotation.getZ()));
+            armorStandMeta.setHeadRotation(rotationRadians);
         }
     }
 
     public Vector3d calculatePosition() {
         WrapperLivingEntity entity = followerEntity.getEntity();
-        Vector3d position = entity.getLocation().getPosition();
-        Vector3d difference = getDifference(player, entity);
+        double speed = Followers.getInstance().getConfigManager().getSpeed();
 
         // Calculates new location of entity based off of the distance to the player
+        Vector3d position = entity.getLocation().getPosition();
+        Vector3d difference = getDifference(position, followerEntity.getTarget()); // TODO: Work out how to get entity eye location from entity
         if (new Vector3d(difference.getX(), 0 , difference.getZ()).lengthSquared() < 6.25) {
             double differenceY = difference.getY() - (Followers.getInstance().getConfigManager().areHitboxesEnabled() ? 0.25 : 0.7);
             position = position.add(new Vector3d(0, differenceY * speed, 0));
@@ -83,29 +67,33 @@ public class MovementTask extends FollowerTask {
             position = position.add(normalizedDifference.multiply(speed * distance));
         }
 
-        return position.add(0, calculateYOffset(entity), 0);
+        // Adds y offset of entity (Bobbing animation)
+        position = position.add(0, calculateYOffset(entity), 0);
+
+        return position;
     }
 
     /**
      * Calculate the rotation of the entity
-     * @return A pair of yaw and pitch as floats
+     * @return A rotation vector of pitch, yaw and roll
      */
-    // TODO: Consider changing to return Vector3f(pitch, yaw?, roll?)
-    public Pair<Float, Float> calculateRotation() {
+    public Vector3f calculateRotation() {
         WrapperLivingEntity entity = followerEntity.getEntity();
 
-        float pitch = getPitch(player, entity);
-        return new Pair<>(0f, pitch);
+        // TODO: Work out how to get entity eye location from entity
+        Vector3d difference = getDifference(entity.getLocation().getPosition(), followerEntity.getTarget());
+        float pitch = getPitch(difference);
+        if (pitch > 60 && pitch < 290) {
+            pitch = pitch <= 175 ? 60 : 290;
+        }
+        float yaw = getYaw(difference);
+
+        return new Vector3f(pitch, yaw, 0);
     }
 
     @Override
     public String getIdentifier() {
         return TaskId.MOVEMENT;
-    }
-
-    @Override
-    public int getDelay() {
-        return 0;
     }
 
     @Override
@@ -117,24 +105,21 @@ public class MovementTask extends FollowerTask {
         return (Math.PI / 60) * Math.sin(((double) 1/30) * Math.PI * (Followers.getInstance().getCurrentTick() + entity.getEntityId()));
     }
 
-    private static float getPitch(Player player, WrapperLivingEntity entity) {
-        // TODO: Work out how to getEyeLocation
-        // Vector difference = (player.getEyeLocation().subtract(0,0.9, 0)).subtract(entity.getEyeLocation()).toVector();
-        Vector3d difference = SpigotConversionUtil.fromBukkitLocation(player.getEyeLocation().subtract(0, 0.9, 0)).getPosition().subtract(entity.getLocation().getPosition());
+    private static float getYaw(Vector3d difference) {
+        double yawRadians = Math.atan2(difference.getX(), difference.getZ());
+        return (float) -Math.toDegrees(yawRadians);
+    }
+
+    private static float getPitch(Vector3d difference) {
         if (difference.getX() == 0.0D && difference.getZ() == 0.0D) {
             return (float) (difference.getY() > 0.0D ? -90 : 90);
         } else {
-            return (float) Math.atan(-difference.getY() / Math.sqrt((difference.getX()*difference.getX()) + (difference.getZ()*difference.getZ())));
+            double pitchRadians = Math.atan(-difference.getY() / Math.sqrt((difference.getX() * difference.getX()) + (difference.getZ() * difference.getZ())));
+            return (float) Math.toDegrees(pitchRadians);
         }
     }
 
-    private static Vector3d getDifference(Player player, WrapperLivingEntity entity) {
-        // TODO: Work out how to getEyeLocation
-        // Vector vector = player.getEyeLocation().subtract(entity.getEyeLocation()).toVector();
-        return SpigotConversionUtil.fromBukkitLocation(player.getEyeLocation()).getPosition().subtract(entity.getLocation().getPosition());
-    }
-
-    private static double eulerToDegree(double euler) {
-        return (euler / (2 * Math.PI)) * 360;
+    private static Vector3d getDifference(Vector3d from, Vector3d to) {
+        return to.subtract(from);
     }
 }
