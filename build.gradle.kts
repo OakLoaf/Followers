2b4907e153599a91cd3fb2161951b87be3cfd5dd
Oak <cooldaveb@live.co.uk>
2024-06-26 18:30:04 +0100

    Prepared for maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,12 @@ dependencies {
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+
+    registerFeature("optional") {
+        usingSourceSet(sourceSets["main"])
+    }
+
+    withSourcesJar()
 }
 
 tasks {
@@ -73,6 +79,39 @@ tasks {
         inputs.property("version", rootProject.version)
         filesMatching("plugin.yml") {
             expand("version" to rootProject.version)
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "lushReleases"
+            url = uri("https://repo.lushplugins.org/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        maven {
+            name = "lushSnapshots"
+            url = uri("https://repo.lushplugins.org/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rootProject.group.toString()
+            artifactId = rootProject.name
+            version = rootProject.version.toString()
+            from(project.components["java"])
         }
     }
 }
