0cf8a0e5c09049ed0d1be3ece4521a6cf642b3d9
Oak <cooldaveb@live.co.uk>
2024-07-24 21:57:50 +0100

    Made armor stand heads.. work?

--- src/main/java/org/lushplugins/followers/entity/tasks/MoveToTask.java
+++ src/main/java/org/lushplugins/followers/entity/tasks/MoveToTask.java
@@ -39,7 +39,7 @@ public class MoveToTask extends FollowerTask {
 
         if (entity.getEntityMeta() instanceof ArmorStandMeta armorStandMeta) {
             Vector3f rotationRadians = new Vector3f((float) Math.toRadians(rotation.getX()), (float) Math.toRadians(rotation.getY()), (float) Math.toRadians(rotation.getZ()));
-            armorStandMeta.setHeadRotation(rotationRadians);
+            armorStandMeta.setHeadRotation(new Vector3f(rotation.getX(), (float) Math.toRadians(rotation.getY()), 0)); // TODO: Make it make sense
         }
     }
 
