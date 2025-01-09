0fabf482bb49e16dbc3b55ebac749e168a12513e
OakLoaf <oak@beaconstudios.org>
2025-01-09 21:15:31 +0000

    Updated LushLib

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 
     // Libraries
     api("me.tofaa.entitylib:spigot:2.4.11-SNAPSHOT")
-    implementation("org.lushplugins:LushLib:0.10.31")
+    implementation("org.lushplugins:LushLib:0.10.34")
     implementation("org.mineskin:java-client:1.2.4-SNAPSHOT")
 }
 
